857886060a5f7991d548570d37bf04989a050466
AnJiYong <97158431+AnJiYong@users.noreply.github.com>
2022-01-11 20:14:32 +0900

    안지용 : 플레이레벨 프리카메라

--- GameLogic/PlayLevel.cpp
+++ GameLogic/PlayLevel.cpp
@@ -2,6 +2,7 @@
 #include "PlayLevel.h"
 #include <GameEngineLevelManager.h>
 #include <GameEngineInput.h>
+#include <GameEngineLevel.h>
 PlayLevel::PlayLevel() // default constructer 디폴트 생성자
 {
 
@@ -24,6 +25,24 @@ void PlayLevel::Loading()
 		GameEngineInput::GetInst().CreateKey("Debug_Next", 'P');
 	}
 	CreateActor<MapTrain>();
+
+
+	if (false == GameEngineInput::GetInst().IsKey("Up"))
+	{
+		GameEngineInput::GetInst().CreateKey("Up", 'w');
+	}
+	if (false == GameEngineInput::GetInst().IsKey("Down"))
+	{
+		GameEngineInput::GetInst().CreateKey("Down", 's');
+	}
+	if (false == GameEngineInput::GetInst().IsKey("Left"))
+	{
+		GameEngineInput::GetInst().CreateKey("Left", 'a');
+	}
+	if (false == GameEngineInput::GetInst().IsKey("Right"))
+	{
+		GameEngineInput::GetInst().CreateKey("Right", 'd');
+	}
 }
 
 void PlayLevel::LevelUpdate()
@@ -31,5 +50,22 @@ void PlayLevel::LevelUpdate()
 	if (true == GameEngineInput::GetInst().IsDown("Debug_Next"))
 	{
 		GameEngineLevelManager::GetInst().ChangeLevel("LobbyLevel");
+	}
+
+	if (true == GameEngineInput::GetInst().IsPress("Up"))
+	{
+		GameEngineLevel::SetCamMove(float4::UP);
+	}
+	if (true == GameEngineInput::GetInst().IsPress("Down"))
+	{
+		GameEngineLevel::SetCamMove(float4::DOWN);
+	}
+	if (true == GameEngineInput::GetInst().IsPress("Left"))
+	{
+		GameEngineLevel::SetCamMove(float4::LEFT);
+	}
+	if (true == GameEngineInput::GetInst().IsPress("Right"))
+	{
+		GameEngineLevel::SetCamMove(float4::RIGHT);
 	}
 }
