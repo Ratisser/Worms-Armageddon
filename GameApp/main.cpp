5684fee6407b75b14f4cfda3cb6181a0ddcb495f
Ratisser <w_vista@naver.com>
2022-01-12 18:08:27 +0900

    조규현 : 충돌 체크용 enum 빠진 파일 추가

--- GameApp/main.cpp
+++ GameApp/main.cpp
@@ -49,8 +49,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 	GameEngineLevelManager::GetInst().CreateLevel<PlayLevel>("PlayLevel");
 
 	// 최초 씬 지정.
-	//GameEngineLevelManager::GetInst().ChangeLevel("TitleLevel");
-	GameEngineLevelManager::GetInst().ChangeLevel("PlayLevel");
+	GameEngineLevelManager::GetInst().ChangeLevel("TitleLevel");
 
 	GameEngineWindow::GetInst().Loop(UpdateFunc);
 
