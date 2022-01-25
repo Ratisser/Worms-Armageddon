19c038b8732ab5167846abadbccab95fc39c001b
Ratisser <w_vista@naver.com>
2022-01-25 15:57:04 +0900

    조규현 : 슈퍼양 리소스 준비

---
+++ GameLogic/SuperSheep.cpp
@@ -0,0 +1,34 @@
+#include "SuperSheep.h"
+
+SuperSheep::SuperSheep()
+	: state_(this)
+{
+
+}
+
+SuperSheep::~SuperSheep()
+{
+
+}
+
+void SuperSheep::Start()
+{
+}
+
+void SuperSheep::UpdateBefore()
+{
+}
+
+void SuperSheep::Update()
+{
+}
+
+void SuperSheep::UpdateAfter()
+{
+}
+
+void SuperSheep::Render()
+{
+}
+
+
